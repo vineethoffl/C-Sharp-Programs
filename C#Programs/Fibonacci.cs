d34ae4ef2a67fd2bbfabd3e3ad6545a83b479a72
Vineeth S <svineethsaji@gmail.com>
2023-11-27 23:15:02 +0530

    #Fibonacci and #PrimeNumber Modification

--- C#Programs/Fibonacci.cs
+++ C#Programs/Fibonacci.cs
@@ -16,7 +16,7 @@ namespace C_Programs
             Console.WriteLine("");
             int n1 = 0, n2 = 1, n3, number;
 
-            Console.WriteLine("Enter the number of elements");
+            Console.Write("Enter the number of elements:");
             number = int.Parse(Console.ReadLine());
             Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
             for (int i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
@@ -26,6 +26,7 @@ namespace C_Programs
                 n1 = n2;
                 n2 = n3;
             }
+            Console.Read();
         }
     }
 }
--- C#Programs/PrimeNumber.cs
+++ C#Programs/PrimeNumber.cs
@@ -10,22 +10,22 @@ namespace C_Programs
     {
         public static void Prime()
         {
-            int n, i, m = 0, flag = 0;
+            int number, i, m, flag = 0;
             Console.Write("Enter the Number to check Prime: ");
-            n = int.Parse(Console.ReadLine());
+            number = int.Parse(Console.ReadLine());
             // Omit 0 and 1 as they are neither prime nor composite
           
-            m = n / 2;
+            m = number / 2;
             for (i = 2; i <= m; i++)
             {
-                if (n % i == 0)
+                if (number % i == 0)
                 {
                     Console.Write("Number is not Prime.");
                     flag = 1;
                     break;
                 }
             }
-            if (n == 1 || n == 0)
+            if (number == 1 || number == 0)
             {
                 Console.Write("Neither prime nor composite");
             }
